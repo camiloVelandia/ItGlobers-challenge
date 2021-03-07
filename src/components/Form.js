0b3d1075ed7f8ef2c5c0e14d1a9e8f693f23d4e0
Camilo Velandia <velandiacamilo356@gmail.com>
2021-03-07 14:30:40 -0500

    add submit info

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useRef, useContext } from 'react';
 import PageContext from '../context/PageContext';
+import '../styles/components/Form.css';
 
 const Form = () => {
   const menuItemActive = useContext(PageContext);
+  const form = useRef(null);
   const range = [];
 
   for (let i = 18; i <= 100; i++) {
@@ -10,25 +12,53 @@ const Form = () => {
     range.push(number);
   }
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = new FormData(form.current);
+    const info = {
+      name: formData.get('nombre'),
+      email: formData.get('email'),
+      celular: formData.get('celular'),
+      edad: formData.get('edad'),
+    };
+    console.log(info);
+  };
+
   return (
-    <section>
-      <h1>
+    <section className="form">
+      <h1 className="form__head">
         Hola, bienvenido, sabemos que quieres viajar en {menuItemActive.menu},
         por favor diligencia el siguiente formulario:
       </h1>
-      <form>
-        <label htmlFor="nombre">Nombre completo</label>
-        <input type="text" name="nombre" id="nombre" />
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="celular">Celular</label>
-        <input type="number" name="celular" id="celular" />
-        <label htmlFor="edad">Edad</label>
-        <select name="" name="edad" id="edad">
-          {range.map(number => (
-            <option key={number}>{number}</option>
-          ))}
-        </select>
+      <form className="form__container" ref={form} onSubmit={handleSubmit}>
+        <div className="form__group">
+          <label htmlFor="nombre">Nombre completo</label>
+          <input type="text" name="nombre" id="nombre" required />
+        </div>
+
+        <div className="form__group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+
+        <div className="form__group">
+          <label htmlFor="celular">Celular</label>
+          <input type="number" name="celular" id="celular" required />
+        </div>
+
+        <div className="form__group">
+          <label htmlFor="edad">Edad</label>
+          <select name="" name="edad" id="edad" required>
+            <option value="">seleccione edad</option>
+            {range.map(number => (
+              <option key={number} value={number}>
+                {number}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <input className="form__submit" type="submit" value="Enviar" />
       </form>
     </section>
   );
